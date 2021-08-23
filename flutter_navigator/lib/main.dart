ee4cafd3e6a667ea14171d532a268d446c834d9e
InJoon-L <dlswns619@naver.com>
2021-08-23 19:29:30 +0900

    initialRoute와 routes를 이용한 페이지 이동

--- flutter_navigator/lib/main.dart
+++ flutter_navigator/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'ScreenA.dart';
+import 'ScreenB.dart';
+import 'ScreenC.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -10,54 +14,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Navigator',
-      home: MyPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ScreenA(),
+        '/b': (context) => ScreenB(),
+        '/c': (context) => ScreenC(),
+      },
     );
   }
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({ Key? key }) : super(key: key);
+// class MyPage extends StatelessWidget {
+//   const MyPage({ Key? key }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the Second page'),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(
-              // builder: (BuildContext context) {
-              //   return SecondPage();
-              // } 
-              builder: (context) => SecondPage()
-            )); // 첫번째 인자 현재 위치
-          },
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('First page'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text('Go to the Second page'),
+//           onPressed: () {
+//             Navigator.push(context, MaterialPageRoute(
+//               // builder: (BuildContext context) {
+//               //   return SecondPage();
+//               // } 
+//               builder: (_) => SecondPage()
+//             )); // 첫번째 인자 현재 위치
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({ Key? key }) : super(key: key);
+// class SecondPage extends StatelessWidget {
+//   const SecondPage({ Key? key }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the First page'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Second page'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text('Go to the First page'),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
