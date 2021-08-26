ad742f48a32ddfbf327f1d9243e9361bdf2715f0
InJoon-L <dlswns619@naver.com>
2021-08-26 19:25:02 +0900

    login 틀 제작

--- flutter_dice_game/lib/main.dart
+++ flutter_dice_game/lib/main.dart
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +36,69 @@ class LogIn extends StatelessWidget {
               onPressed: (){}
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 50.0)),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170.0,
+                height: 190.0,
+              ),
+            ),
+            Form(
+              child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15.0
+                    )
+                  )
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "dice"'
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "Password"'
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 40.0,),
+                      ButtonTheme(
+                        minWidth: 100.0,
+                        height: 50.0,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.orangeAccent
+                          ),
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 35.0,
+                          ),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
